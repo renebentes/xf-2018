ff52c23ee18a8ed2964e816f79d444e46c566247
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-10-11 18:23:26 -0300

    :sparkles: Adiciona validação por expressão regular

--- ConsultaCep/ConsultaCep/Pages/MainPage.xaml.cs
+++ ConsultaCep/ConsultaCep/Pages/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ConsultaCep.Services;
+using System.Text.RegularExpressions;
 using Xamarin.Forms;
 
 namespace ConsultaCep.Pages
@@ -19,8 +20,18 @@ namespace ConsultaCep.Pages
         private async void OnBuscarClicked(object sender, System.EventArgs e)
         {
             string cep = Cep.Text.Trim();
-            Model.Endereco endereco = await _service.ObterEndereco(cep);
-            Resultado.Text = $"Endereço: {endereco.Localidade} - {endereco.Uf}, {endereco.Logradouro} - {endereco.Bairro}";
+
+            if (Validate(cep))
+            {
+                Model.Endereco endereco = await _service.ObterEndereco(cep);
+                Resultado.Text = $"Endereço: {endereco.Localidade} - {endereco.Uf}, {endereco.Logradouro} - {endereco.Bairro}";
+            }
+            else
+            {
+                await DisplayAlert("ERRO", "O CEP informado é inválido", "Ok").ConfigureAwait(false);
+            }
         }
+
+        private bool Validate(string cep) => Regex.IsMatch(cep ?? "", "[0-9]{8}");
     }
 }

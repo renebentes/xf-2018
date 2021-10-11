e714890a977e47affa6f17ee8c40aa3126e2314c
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-10-11 16:54:04 -0300

    :sparkles: Integra tela ao serviço de consultar CEP

--- ConsultaCep/ConsultaCep/Pages/MainPage.xaml.cs
+++ ConsultaCep/ConsultaCep/Pages/MainPage.xaml.cs
@@ -1,9 +1,26 @@
-﻿using Xamarin.Forms;
+﻿using ConsultaCep.Services;
+using Xamarin.Forms;
 
 namespace ConsultaCep.Pages
 {
     public partial class MainPage : ContentPage
     {
-        public MainPage() => InitializeComponent();
+        private readonly ICepService _service;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _service = new HttpClientService();
+
+            Buscar.Clicked += OnBuscarClicked;
+        }
+
+        private async void OnBuscarClicked(object sender, System.EventArgs e)
+        {
+            string cep = Cep.Text.Trim();
+            Model.Endereco endereco = await _service.ObterEndereco(cep);
+            Resultado.Text = $"Endereço: {endereco.Localidade} - {endereco.Uf}, {endereco.Logradouro} - {endereco.Bairro}";
+        }
     }
 }

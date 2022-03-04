d710d6f961e8aec5924007be6274c799109e5bdd
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-03-03 22:06:36 -0300

    feat: Exibe página com Entry e Editor

---
+++ XamControle/XamControle/Pages/EntryEditorPage.xaml.cs
@@ -0,0 +1,19 @@
+using Xamarin.Forms;
+
+namespace XamControle.Pages;
+
+public partial class EntryEditorPage : ContentPage
+{
+    public EntryEditorPage()
+    {
+        InitializeComponent();
+
+        Idade.TextChanged += (s, e) => {
+            Duplicado.Text = Idade.Text;
+        };
+
+        Comentario.Completed += (s, e) => {
+            QuantidadeCaracteres.Text = Comentario.Text.Length.ToString();
+        };
+    }
+}

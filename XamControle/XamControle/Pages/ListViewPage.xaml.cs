ce04c10072d1b64ce422580d919960eb9c1b1008
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-04-09 10:57:14 -0300

    feat: Define dados de exemplo

--- XamControle/XamControle/Pages/ListViewPage.xaml.cs
+++ XamControle/XamControle/Pages/ListViewPage.xaml.cs
@@ -1,8 +1,18 @@
+using System.Collections.Generic;
 using Xamarin.Forms;
+using XamControle.Models;
 
 namespace XamControle.Pages;
 
 public partial class ListViewPage : ContentPage
 {
     public ListViewPage() => InitializeComponent();
+
+    public IList<Pessoa> Pessoas { get; } = new List<Pessoa>
+    {
+        new Pessoa { Nome = "José", Idade = 20 },
+        new Pessoa { Nome = "Maria", Idade = 23 },
+        new Pessoa { Nome = "Felipe", Idade = 25 },
+        new Pessoa { Nome = "João", Idade = 30 }
+    };
 }

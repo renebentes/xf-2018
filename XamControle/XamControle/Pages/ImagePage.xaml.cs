72b391c7c98ba4ce914ac12d8e5a88ba7b219409
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-04-09 09:52:44 -0300

    feat: Adiciona elemento Image no code-behind

--- XamControle/XamControle/Pages/ImagePage.xaml.cs
+++ XamControle/XamControle/Pages/ImagePage.xaml.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using Xamarin.Forms;
 
 namespace XamControle.Pages;
@@ -5,4 +6,22 @@ namespace XamControle.Pages;
 public partial class ImagePage : ContentPage
 {
     public ImagePage() => InitializeComponent();
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        var imagePath = "usb.jpg";
+
+        if (Device.RuntimePlatform == Device.UWP)
+        {
+            imagePath = Path.Combine("Assets", imagePath);
+        }
+
+        var image = new Image {
+            Source = imagePath
+        };
+
+        container.Children.Add(image);
+    }
 }

059d6e63d8a1922bb1d17473bf067a7e1557bd2f
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-12-27 16:21:05 -0300

    feat: Obtém a prioridade da tarefa com base na classe de estilo

--- Tarefas/Tarefas/Pages/AddPage.xaml.cs
+++ Tarefas/Tarefas/Pages/AddPage.xaml.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using Tarefas.Enums;
 using Xamarin.Forms;
 
 namespace Tarefas.Pages
@@ -6,15 +8,39 @@ namespace Tarefas.Pages
     public partial class AddPage : ContentPage
     {
         private Label _lastTapped = new Label();
+        private Priority _priority;
 
         public AddPage()
             => InitializeComponent();
 
+        private void GetPriority(BoxView boxView)
+        {
+            var priority = boxView
+                            .StyleClass
+                            .LastOrDefault(style => style.Contains("Priority"))?
+                            .Replace("Priority", string.Empty) ?? "Low";
+
+            _priority = (Priority)Enum.Parse(typeof(Priority), priority);
+        }
+
         private void OnTapped(object sender, EventArgs e)
         {
-            if (sender is not FlexLayout flexLayout) return;
+            if (sender is not FlexLayout flexLayout)
+            {
+                return;
+            }
 
-            if (flexLayout.Children[1] is not Label label || label == _lastTapped) return;
+            if (flexLayout.Children[0] is not BoxView boxView)
+            {
+                return;
+            }
+
+            if (flexLayout.Children[1] is not Label label || label == _lastTapped)
+            {
+                return;
+            }
+
+            GetPriority(boxView);
 
             VisualStateManager.GoToState(_lastTapped, "Normal");
             VisualStateManager.GoToState(label, "Selected");

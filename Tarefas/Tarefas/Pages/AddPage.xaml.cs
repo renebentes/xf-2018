8c1f3aeff188b8d776292562f6f8eb1e6fc9ea94
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-12-27 11:39:33 -0300

    feat: Exibe texto em preto quando selecionado

--- Tarefas/Tarefas/Pages/AddPage.xaml.cs
+++ Tarefas/Tarefas/Pages/AddPage.xaml.cs
@@ -1,10 +1,24 @@
+using System;
 using Xamarin.Forms;
 
 namespace Tarefas.Pages
 {
     public partial class AddPage : ContentPage
     {
+        private Label _lastTapped = new Label();
+
         public AddPage()
             => InitializeComponent();
+
+        private void OnTapped(object sender, EventArgs e)
+        {
+            if (sender is not FlexLayout flexLayout) return;
+
+            if (flexLayout.Children[1] is not Label label || label == _lastTapped) return;
+
+            VisualStateManager.GoToState(_lastTapped, "Normal");
+            VisualStateManager.GoToState(label, "Selected");
+            _lastTapped = label;
+        }
     }
 }

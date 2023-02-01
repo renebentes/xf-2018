a123f8e6cebbb5346e58f3f734fa3f7c9c58c5e4
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-01-31 21:43:29 -0300

    styles: Aplica padrões de codificação

--- XamCell/XamCell/Pages/EntryCellPage.xaml.cs
+++ XamCell/XamCell/Pages/EntryCellPage.xaml.cs
@@ -1,20 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace XamCell.Pages
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class EntryCellPage : ContentPage
     {
         public EntryCellPage()
-        {
-            InitializeComponent();
-        }
+            => InitializeComponent();
     }
 }
--- XamCell/XamCell/Pages/SwitchCellPage.xaml.cs
+++ XamCell/XamCell/Pages/SwitchCellPage.xaml.cs
@@ -1,20 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace XamCell.Pages
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SwitchCellPage : ContentPage
     {
         public SwitchCellPage()
-        {
-            InitializeComponent();
-        }
+            => InitializeComponent();
     }
 }

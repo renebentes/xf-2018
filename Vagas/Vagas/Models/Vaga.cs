bbd76906814f8d53439ccf370c05c6e2638bd6b2
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-03-24 11:31:39 -0300

    feat: Define chave primária do modelo de vagas

--- Vagas/Vagas/Models/Vaga.cs
+++ Vagas/Vagas/Models/Vaga.cs
@@ -1,21 +1,26 @@
+using SQLite;
+
 namespace Vagas.Models
 {
-    internal class Vaga
+    public class Vaga
     {
-        public string Cidade { get; set; }
+        public string? Cidade { get; set; }
 
-        public string Descricao { get; set; }
+        public string? Descricao { get; set; }
 
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
-        public string Nome { get; set; }
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string? Nome { get; set; }
 
         public short Quantidade { get; set; }
 
         public double Salario { get; set; }
 
-        public string Telefone { get; set; }
+        public string? Telefone { get; set; }
 
-        public string TipoContratacao { get; set; }
+        public string? TipoContratacao { get; set; }
     }
 }

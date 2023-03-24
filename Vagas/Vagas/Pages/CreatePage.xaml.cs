3562cee638666a6c05a4077fb7c88bfdf299a077
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-03-24 15:34:49 -0300

    feat: Persiste uma vaga no banco

--- Vagas/Vagas/Pages/CreatePage.xaml.cs
+++ Vagas/Vagas/Pages/CreatePage.xaml.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Vagas.Models;
 using Xamarin.Forms;
 
@@ -9,21 +12,34 @@ namespace Vagas.Pages
             => InitializeComponent();
 
         private async void OnSave(object sender, System.EventArgs e)
-        {
-            var vaga = new Vaga
-            {
-                Nome = name.Text,
-                Empresa = company.Text,
-                Quantidade = short.Parse(quantity.Text),
-                Salario = double.Parse(salary.Text),
-                Cidade = city.Text,
-                Descricao = description.Text,
-                TipoContratacao = tipoContratacao.IsToggled ? "PJ" : "CLT",
-                Telefone = telefone.Text,
-                Email = email.Text
-            };
+            => await SaveAsync();
 
-            await App.Database.CreateAsync(vaga);
+        private async Task SaveAsync()
+        {
+            try
+            {
+                var vaga = new Vaga
+                {
+                    Nome = name.Text,
+                    Empresa = company.Text,
+                    Quantidade = short.Parse(quantity.Text),
+                    Salario = double.Parse(salary.Text),
+                    Cidade = city.Text,
+                    Descricao = description.Text,
+                    TipoContratacao = tipoContratacao.IsToggled ? "PJ" : "CLT",
+                    Telefone = telefone.Text,
+                    Email = email.Text
+                };
+
+                await App.Database.CreateAsync(vaga);
+
+                _ = await Navigation.PopAsync().ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
         }
     }
 }

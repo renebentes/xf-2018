5d2ff46cac29a81d97432a5ea192eba61922fc9f
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-05-29 21:07:37 -0300

    feat Aplica atributo CallerMemberName
    O atributo permite identificar a propriedade que
    chamou o método

--- Mimica/Mimica/ViewModels/BaseViewModel.cs
+++ Mimica/Mimica/ViewModels/BaseViewModel.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Mimica.ViewModels
 {
@@ -6,7 +7,7 @@ namespace Mimica.ViewModels
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged(string propertyName)
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }

fad7ae3948a0794a0100c1b8622d043dd8eaf82c
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-06-01 21:15:27 -0300

    feat: Aplica validação básica

--- Mimica/Mimica/ViewModels/MainViewModel.cs
+++ Mimica/Mimica/ViewModels/MainViewModel.cs
@@ -11,7 +11,7 @@ namespace Mimica.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
-        private Level selectedLevel;
+        private string errorMessage;
         private string selectedLevelName;
 
         public MainViewModel()
@@ -20,15 +20,32 @@ namespace Mimica.ViewModels
             Game = new Game
             {
                 PlayerOne = new Player(),
-                PlayerTwo = new Player()
+                PlayerTwo = new Player(),
+                WordTimeInSeconds = 120,
+                Matches = 7
             };
 
             Enum.GetNames(typeof(Level)).ForEach((name) => Levels.Add(name));
         }
 
+        public string ErrorMessage
+        {
+            get => errorMessage;
+            set
+            {
+                errorMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
         public Game Game { get; set; }
 
         public ObservableCollection<string> Levels { get; } = new ObservableCollection<string>();
+
+        public Level SelectedLevel
+            => string.IsNullOrEmpty(selectedLevelName)
+                ? Level.Random
+                : (Level)Enum.Parse(typeof(Level), selectedLevelName, true);
 
         public string SelectedLevelName
         {
@@ -44,19 +61,33 @@ namespace Mimica.ViewModels
             }
         }
 
-        public Level SelectedLevel
-            => string.IsNullOrEmpty(selectedLevelName)
-                ? Level.Random
-                : (Level)Enum.Parse(typeof(Level), selectedLevelName, true);
-
         public ICommand StartGameCommand { get; set; }
 
         private void OnStartGame()
         {
-            DataStore.Game = Game;
-            DataStore.CurrentRound = 1;
-            DataStore.Game.Level = SelectedLevel;
-            Application.Current.MainPage = new GamePage(Game.PlayerOne);
+            if (Validate())
+            {
+                DataStore.Game = Game;
+                DataStore.CurrentRound = 1;
+                DataStore.Game.Level = SelectedLevel;
+                Application.Current.MainPage = new GamePage(Game.PlayerOne);
+            }
+
+            bool Validate()
+            {
+                var message = string.Empty;
+
+                if (Game.WordTimeInSeconds < 10)
+                    message += "O tempo mínimo para uma palavra é 10 segundos";
+
+                if (Game.Matches < 1)
+                    message += "\nIndique o número de rodadas maior que 1";
+
+                if (string.IsNullOrEmpty(message)) return true;
+
+                ErrorMessage = message;
+                return false;
+            }
         }
     }
 }

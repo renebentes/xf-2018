e1257dc60aa610971cf173dc325d1beddde89edb
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-06-05 10:02:42 -0300

    feat: Define cabeçalho para as páginas

---
+++ Mimica/Mimica/Views/HeaderView.xaml.cs
@@ -0,0 +1,11 @@
+using Xamarin.Forms;
+
+namespace Mimica.Views
+{
+    public partial class HeaderView : ContentView
+    {
+        public HeaderView()
+            => InitializeComponent();
+    }
+}
+

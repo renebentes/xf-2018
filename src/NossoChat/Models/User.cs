ce87325556e93826d1d150331cd919a8acca2b80
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-06-20 20:47:41 -0300

    feat: Define serviço para obter usuário

---
+++ src/NossoChat/Models/User.cs
@@ -0,0 +1,11 @@
+namespace NossoChat.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; } = default!;
+
+        public string Password { get; set; } = default!;
+    }
+}
---
+++ src/NossoChat/Services/DataService.cs
@@ -0,0 +1,50 @@
+using NossoChat.Models;
+
+namespace NossoChat.Services
+{
+    public class DataService
+    {
+        private const string BaseUrl = "https://teste.com";
+        private readonly HttpClient _httpClient;
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+        public DataService()
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(BaseUrl)
+            };
+
+            _jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+        }
+
+        public async Task<User> GetUser(User user)
+        {
+            try
+            {
+                var json = JsonSerializer.Serialize(user, _jsonSerializerOptions);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PostAsync("user", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var userContent = await response.Content.ReadAsStringAsync();
+
+                    return JsonSerializer.Deserialize<User>(userContent, _jsonSerializerOptions)
+                           ?? throw new InvalidOperationException();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return new User();
+        }
+    }
+}

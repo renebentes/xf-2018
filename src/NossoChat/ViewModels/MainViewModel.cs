4f8527ae53ee658782f711f03f6d520588ff2bbb
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-06-27 20:25:30 -0300

    feat: Define tela principal do app

--- src/NossoChat/ViewModels/MainViewModel.cs
+++ src/NossoChat/ViewModels/MainViewModel.cs
@@ -2,4 +2,15 @@ namespace NossoChat.ViewModels;
 
 public class MainViewModel : BaseViewModel
 {
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            _name = value;
+            OnPropertyChanged();
+        }
+    }
 }

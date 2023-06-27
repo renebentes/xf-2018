0704f8d8c22a5a17e3e62fd21843ee1b8d05f483
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-06-26 21:36:23 -0300

    feat: Simplifica modelo de mensagens

--- src/NossoChat/Models/Message.cs
+++ src/NossoChat/Models/Message.cs
@@ -2,9 +2,11 @@ namespace NossoChat.Models;
 
 public class Message
 {
+    public int ChatId { get; set; }
+
     public string Content { get; set; }
 
     public int Id { get; set; }
 
-    public User User { get; set; }
+    public int UserId { get; set; }
 }

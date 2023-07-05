f003d64c63057c2ede3e95ee6fd28189f825dcfc
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-07-05 10:36:47 -0300

    feat: Aceita configuração de porta via variável de ambiente

--- api/server.js
+++ api/server.js
@@ -4,8 +4,10 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+const port = process.env.PORT || 3000;
+
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log("JSON Server is running");
 });

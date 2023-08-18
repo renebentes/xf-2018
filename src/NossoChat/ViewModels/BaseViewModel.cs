397555073c80718b4cbc656686722fb64867124f
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-08-17 21:43:02 -0300

    feat: Define manipulador para navegação entre páginas

--- src/NossoChat/ViewModels/BaseViewModel.cs
+++ src/NossoChat/ViewModels/BaseViewModel.cs
@@ -1,5 +1,7 @@
 using NossoChat.Services;
+using System.Windows.Input;
 using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
 
 namespace NossoChat.ViewModels;
 
@@ -8,5 +10,14 @@ public abstract class BaseViewModel : ObservableObject
     protected BaseViewModel()
         => DataService = new DataService();
 
+    public ICommand NavigateCommand
+        => CommandFactory.Create<Page>(page => NavigateTo(page));
+
     protected DataService DataService { get; }
+
+    protected INavigation Navigation
+        => Application.Current.MainPage.Navigation;
+
+    private Task NavigateTo(Page? page)
+        => page is not null ? Navigation.PushAsync(page) : Task.CompletedTask;
 }
